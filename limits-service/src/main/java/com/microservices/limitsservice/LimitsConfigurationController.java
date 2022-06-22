c14c17fa652b09dc4dbd83e65526ad10cf916dc3
prabhat <prabhatproject1@gmail.com>
2022-06-22 14:53:12 +0530

    read limits from properties file instead from hardcoded

--- limits-service/src/main/java/com/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.microservices.limitsservice;
-
+import org.springframework.beans.factory.annotation.Autowired;
+//Note-for reding the values from properties file we can use @Value annotation also but here better approach
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +9,15 @@ import com.microservices.limitsservice.bean.LimitConfiguration;
 @RestController
 
 public class LimitsConfigurationController {
+	@Autowired
+	Configuration configuration;//managed by spring boot
+	
 	@GetMapping("/limits")
 	public LimitConfiguration retrievalLimitsFromConfiguration() {
 		
 		
-		return new LimitConfiguration(1000,1);
+		//return new LimitConfiguration(1000,1);
+		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
 	}
 
 }
